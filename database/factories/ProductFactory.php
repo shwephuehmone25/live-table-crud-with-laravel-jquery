8d7e39ec5f57e5f57016ecf4a94b8be0bc40fc38
shwephuehmone25 <shwephue7889@gmail.com>
2023-03-18 14:26:26 +0630

    add databse seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->regexify('[A-Za-z0-9]{5}'),
+            'price' => $this->faker->numberBetween($min = 1500, $max = 6000), 
+        ];
+    }
+}
